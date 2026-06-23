c50fee6238a47ca3d19e0499e8453338d15c6c59
arturfsouza <artur.fonseca.souza@gmail.com>
2026-06-22 21:53:15 -0300

    Adiciona função de clonagem de repositório

--- repohealth/git_utils.py
+++ repohealth/git_utils.py
@@ -1,5 +1,13 @@
 import tempfile
 
+from git import Repo
+
 
 def create_temp_dir() -> str:
     return tempfile.mkdtemp(prefix="repohealth_")
+
+
+def clone_repository(repo_url: str, destination: str) -> str:
+    Repo.clone_from(repo_url, destination)
+
+    return destination
