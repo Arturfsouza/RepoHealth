c22454e31d2cb0d567234a1fca7a28744004d6cf
arturfsouza <artur.fonseca.souza@gmail.com>
2026-06-22 22:12:32 -0300

    Integra pasta temporária na CLI

--- repohealth/cli.py
+++ repohealth/cli.py
@@ -1,11 +1,16 @@
 import typer
 
+from repohealth.git_utils import create_temp_dir
+
 app = typer.Typer(help="RepoHealth - análise de risco de manutenção em repositórios Git.")
 
 
 @app.command()
 def analyze(repo_url: str):
+    temp_dir = create_temp_dir()
+
     typer.echo(f"Repositório informado: {repo_url}")
+    typer.echo(f"Pasta temporária criada: {temp_dir}")
 
 
 if __name__ == "__main__":
