3d6f61a5ef128808b909882f2016b4017b1b5c81
arturfsouza <artur.fonseca.souza@gmail.com>
2026-06-22 21:43:59 -0300

    Adiciona teste para commit com fix

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -3,3 +3,7 @@ from repohealth.metrics import is_bugfix_commit
 
 def test_is_bugfix_commit_without_bug_keyword():
     assert is_bugfix_commit("add new feature") is False
+
+
+def test_is_bugfix_commit_with_fix_keyword():
+    assert is_bugfix_commit("fix login error") is True
