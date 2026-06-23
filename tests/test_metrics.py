cb8bb78a8cd4284de3c98d34c9b4621315f3526b
arturfsouza <artur.fonseca.souza@gmail.com>
2026-06-22 21:47:10 -0300

    Adiciona teste para cálculo de score

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,5 +1,4 @@
-from repohealth.metrics import is_bugfix_commit
-
+from repohealth.metrics import calculate_risk_score, is_bugfix_commit
 
 def test_is_bugfix_commit_without_bug_keyword():
     assert is_bugfix_commit("add new feature") is False
@@ -14,3 +13,6 @@ def test_is_bugfix_commit_with_bug_keyword():
 
 def test_is_bugfix_commit_with_empty_message():
     assert is_bugfix_commit("") is False
+
+def test_calculate_risk_score():
+    assert calculate_risk_score(commits=10, authors=2, bugfix_commits=3) == 38
