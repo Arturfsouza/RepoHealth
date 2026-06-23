dce346fc63a31eb78638a1b1102ccf8732978f62
arturfsouza <artur.fonseca.souza@gmail.com>
2026-06-22 21:45:00 -0300

    Adiciona teste para commit com bug

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -7,3 +7,7 @@ def test_is_bugfix_commit_without_bug_keyword():
 
 def test_is_bugfix_commit_with_fix_keyword():
     assert is_bugfix_commit("fix login error") is True
+
+
+def test_is_bugfix_commit_with_bug_keyword():
+    assert is_bugfix_commit("bug in payment service") is True
