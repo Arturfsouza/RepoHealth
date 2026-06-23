0e6ba9b0eb28aa5fe84227431683ad08ef4fd55f
arturfsouza <artur.fonseca.souza@gmail.com>
2026-06-22 21:44:24 -0300

    Identifica commits com palavra bug

--- repohealth/metrics.py
+++ repohealth/metrics.py
@@ -2,4 +2,6 @@ def is_bugfix_commit(message: str) -> bool:
     if not message:
         return False
 
-    return "fix" in message.lower()
+    message_lower = message.lower()
+
+    return "fix" in message_lower or "bug" in message_lower
