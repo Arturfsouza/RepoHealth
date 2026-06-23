b02f7cc021ea1d84d6d9f60fd2be980b5e34071a
arturfsouza <artur.fonseca.souza@gmail.com>
2026-06-22 22:12:12 -0300

    Adiciona comando de análise na CLI

--- repohealth/cli.py
+++ repohealth/cli.py
@@ -3,5 +3,10 @@ import typer
 app = typer.Typer(help="RepoHealth - análise de risco de manutenção em repositórios Git.")
 
 
+@app.command()
+def analyze(repo_url: str):
+    typer.echo(f"Repositório informado: {repo_url}")
+
+
 if __name__ == "__main__":
     app()
