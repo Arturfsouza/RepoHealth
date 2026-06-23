a44faf96f21bfbf9268d28ce4ba4b059fc0d24b3
arturfsouza <artur.fonseca.souza@gmail.com>
2026-06-22 22:01:51 -0300

    Prepara estrutura de métricas por arquivo

--- repohealth/analyzer.py
+++ repohealth/analyzer.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from pathlib import Path
 
 from pydriller import Repository
@@ -9,5 +10,11 @@ def analyze_repository(repo_path: str) -> list[dict]:
 
     if not Path(repo_path).exists():
         raise FileNotFoundError("O caminho informado não existe.")
+
+    files_data = defaultdict(lambda: {
+        "commits": 0,
+        "authors": set(),
+        "bugfix_commits": 0
+    })
 
     return []
