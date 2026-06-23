87ccfd956f8e3094d891272c0e7927c6cc913679
arturfsouza <artur.fonseca.souza@gmail.com>
2026-06-22 22:12:46 -0300

    Integra clonagem de repositório na CLI

--- repohealth/cli.py
+++ repohealth/cli.py
@@ -1,6 +1,6 @@
 import typer
 
-from repohealth.git_utils import create_temp_dir
+from repohealth.git_utils import clone_repository, create_temp_dir
 
 app = typer.Typer(help="RepoHealth - análise de risco de manutenção em repositórios Git.")
 
@@ -9,8 +9,10 @@ app = typer.Typer(help="RepoHealth - análise de risco de manutenção em reposi
 def analyze(repo_url: str):
     temp_dir = create_temp_dir()
 
-    typer.echo(f"Repositório informado: {repo_url}")
-    typer.echo(f"Pasta temporária criada: {temp_dir}")
+    typer.echo("Clonando repositório...")
+    clone_repository(repo_url, temp_dir)
+
+    typer.echo(f"Repositório clonado em: {temp_dir}")
 
 
 if __name__ == "__main__":
